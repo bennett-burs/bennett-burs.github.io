ce7273d35ab4dc042ccc7a23902a0e5205382516
Amartya Saran <amartyasaran@gmail.com>
2023-03-30 17:35:41 +0530

    .

--- test.js
+++ test.js
@@ -1,17 +1,36 @@
-function clickedBtn() {
-  alert(event.target.id);
-}
-var modal = document.getElementById("myModal");
-var btn = document.getElementById(clickedBtn());
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-span.onclick = function () {
-  modal.style.display = "none";
-};
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function myFunction(this) {
+  var x = button.id;
+
+  var btn = document.getElementById(hover(x));
+  var y = "";
+
+  if (btn == "one") {
+    y = "first";
+  } else if (btn == "two") {
+    y = "second";
+  } else if (btn == "three") {
+    y = "third";
+  } else {
+    console.log("Unknown");
   }
-};
+  var modal = document.getElementById(x);
+
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+}
+
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("myBtn");
+// var span = document.getElementsByClassName("close")[0];
+// btn.onclick = function () {
+//   modal.style.display = "block";
+// };
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
