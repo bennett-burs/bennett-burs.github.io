f7294c60b96e0e12233d04cf148a3ad5c8fff286
ShreejeetPurwar <98033936+ShreejeetPurwar@users.noreply.github.com>
2023-03-31 10:39:46 +0530

    responsive done, mobile page optimised

--- test.js
+++ test.js
@@ -15,3 +15,11 @@
 //     modal.style.display = "none";
 //   }
 // };
+
+$(document).ready(function () {
+    $('#dtDynamicVerticalScrollExample').DataTable({
+      "scrollY": "50vh",
+      "scrollCollapse": true,
+    });
+    $('.dataTables_length').addClass('bs-select');
+  });
